d732029c6aee79a626f17839684f5ccd5a6633dc
Aman0246 <amankashyap0246@gmail.com>
2023-06-13 16:40:01 +0530

    final

--- controllers/InternControllers.js
+++ controllers/InternControllers.js
@@ -1,10 +1,14 @@
 const {InternModel}=require("../Models/InternModel")
+const {CollegeModel}=require("../Models/CollegeModel")
+
 const { isValidObjectId } = require("mongoose")
 const createIntern=async(req,res)=>{
-    const {name,email,mobile,collegeId}=req.body
+    const {name,email,mobile,collageName}=req.body
     try {
         if(!isValidObjectId(collegeId))return res.status(400).send({status:false,message:"college Id is invalid"})
-        const data= await InternModel.create({name,email,mobile,collegeId})
+         const collage= await CollegeModel.findOne({name:collageName})
+         if(!collage) return res.status(404).send({status:false,message:"collage not exist"})
+        const data= await InternModel.create({name,email,mobile,collegeId:collage._id})
         const datafind=await InternModel.findOne({email})
         if(datafind)return res.status(400).send({status:false,message:"collage alredy exist"})
         res.status(201).send({status:true,message:"data",data:data})

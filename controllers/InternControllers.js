01000677ba2099fe8870af55bb6a01d212a3f0b4
Aman0246 <amankashyap0246@gmail.com>
2023-06-13 16:07:22 +0530

    final

--- controllers/InternControllers.js
+++ controllers/InternControllers.js
@@ -5,6 +5,8 @@ const createIntern=async(req,res)=>{
     try {
         if(!isValidObjectId(collegeId))return res.status(400).send({status:false,message:"college Id is invalid"})
         const data= await InternModel.create({name,email,mobile,collegeId})
+        const datafind=await InternModel.findOne({email})
+        if(datafind)return res.status(400).send({status:false,message:"collage alredy exist"})
         res.status(201).send({status:true,message:"data",data:data})
     } catch (error) {
         res.status(400).send({status:false,message:error.message})

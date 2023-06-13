ea806ddc8dc8c5b06e5e65e1319b6675bb1b36fb
Aman0246 <amankashyap0246@gmail.com>
2023-06-13 17:21:10 +0530

    allDone

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 const express = require('express');
-const bodyParser = require ('body-parser');
+const app = express()
 const route =  require("./routes/route");
 require('dotenv').config()
 
-const {default : mongoose}  = require('mongoose');
-const app = express()
-
+const mongoose  = require('mongoose');
 
 app.use(express.json())
 
-
-
 mongoose.connect(process.env.MONGOCONNECT).then(()=> {
-    console.log("MongoDB is Connected");
-
-}).catch((err) => {
-console.log(err);
-})
+    console.log("MongoDB is Connected")}).catch(() => {console.log("MongoDB not Connected")})
 app.use('/',route)
 
-
-app.listen(process.env.PORT||3000,()=>{
-    console.log(`Server is running on port ${process.env.PORT||3000}`)
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,17 +3,8 @@ const router = express.Router()
 const {createCollegeDoc} = require('../controllers/collegeController')
 const {createIntern,getIntern} = require('../controllers/internController')
 
-
-
-//for test
-router.get('/', (req,res)=>{
-    res.send("ok")
-})
-
-// create college profile
 router.post('/functionup/colleges', createCollegeDoc )
 
-// creaet intern 
 router.post('/functionup/interns',createIntern )
 
 router.get('/functionup/collegeDetails',getIntern )
